cc06412c6169258a5b960714a9de358c9f009fd5
Denis Pitsul <denia.pitsul@gmail.com>
2023-09-22 14:01:27 +0300

    homework 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem(CURRENT_TIME_KEY, data.seconds.toString());
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY) || "0");
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import { throttle } from 'lodash';
 
+const FORM_SATE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+const { email, message } = feedbackForm.elements;
+let formData = {};
+onLoadPage();
+
+feedbackForm.addEventListener('input', throttle(onInputChange, 500));
+feedbackForm.addEventListener('submit', onFormSubmit)
+
+function onInputChange() {
+    formData = {
+        email: email.value,
+        message: message.value
+    };
+    localStorage.setItem(FORM_SATE_KEY, JSON.stringify(formData));
+}
+
+function onLoadPage() {
+    formData = JSON.parse(localStorage.getItem(FORM_SATE_KEY));
+    if(formData) {
+        email.value = formData.email || '';
+        message.value = formData.message || '';
+    }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    if(!email.value.trim() || !message.value.trim())
+        return allert('Email or message is empty!');
+
+    console.log({
+        email: email.value,
+        message: message.value
+    });
+    localStorage.clear();
+    e.currentTarget.reset();
+    formData = {};
+}
